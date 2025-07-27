a32145c751ed9f0e963f8911efc15d102226515c
Never249 <arthSchmidt@yandex.ru>
2025-07-27 17:07:19 +0700

    добвил спавн противников

--- Main/scripts/main_game.gd
+++ Main/scripts/main_game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 @onready var enemy = preload("res://Main/Scenes/enemy_spawn.tscn")
 @onready var EnemyPath = preload("res://Main/Scenes/enemy_paths.tscn")
+@onready var EnemyPath2 = preload("res://Main/Scenes/enemy_paths2.tscn")
+@onready var EnemyPath3 = preload("res://Main/Scenes/enemy_paths3.tscn")
 @onready var path_2d: Path2D = $Path2D
 
 
@@ -48,3 +50,20 @@ func _on_cycle_timer_timeout() -> void:
 func check_health():
 	if glob.health <= 0:
 		get_tree().change_scene_to_file("res://Main/Scenes/game_over.tscn")
+
+
+func _on_click_timer_timeout() -> void:
+	glob.klicks+=1
+
+
+func _on_second_enemy_timer_timeout() -> void:
+	var Spawn_Enemy= EnemyPath2.instantiate()
+	#Spawn_Enemy.Enemy=enemy
+	path_2d.add_child(Spawn_Enemy)
+
+
+
+func _on_third_enemy_timer_timeout() -> void:
+	var Spawn_Enemy= EnemyPath3.instantiate()
+	#Spawn_Enemy.Enemy=enemy
+	path_2d.add_child(Spawn_Enemy)
